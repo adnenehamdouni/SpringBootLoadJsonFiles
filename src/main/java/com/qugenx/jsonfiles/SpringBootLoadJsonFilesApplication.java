40789ec377ff63457d87afbf7f3fef6fb0321c12
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-11-30 09:30:57 +0100

    first commit

---
+++ src/main/java/com/qugenx/jsonfiles/SpringBootLoadJsonFilesApplication.java
@@ -0,0 +1,13 @@
+package com.qugenx.jsonfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoadJsonFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoadJsonFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qugenx/jsonfiles/SpringBootLoadJsonFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qugenx.jsonfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoadJsonFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
